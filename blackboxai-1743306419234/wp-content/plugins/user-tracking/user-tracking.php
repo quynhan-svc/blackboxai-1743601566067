5039e3833463b95a912b4851686c4b05829d4c03
quynhan-svc <duyhau1610@gmail.com>
2025-04-02 21:41:20 +0700

    Add blackboxai-1743306419234/wp-content/plugins/user-tracking/user-tracking.php 1743604880569

--- blackboxai-1743306419234/wp-content/plugins/user-tracking/user-tracking.php
+++ blackboxai-1743306419234/wp-content/plugins/user-tracking/user-tracking.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: User Tracking for Google Ads
  * Description: Tracks user behavior from Google Ads and detects fraudulent clicks.
- * Version: 1.0.4
+ * Version: 1.0.5
  * Requires at least: 6.7.2
  * Requires PHP: 8.0
  * Author: Quỳnh An Solar Nha Trang
